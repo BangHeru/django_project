b3c4b5d81770d20105bf2b7232661a7df49185d0
heru <bang.heru82@gmail.com>
2018-12-04 00:12:13 +0700

    modif urls and views on blog app

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def home(req):
+    return HttpResponse('<h1>Blog Home</h1>')
+
+def about(req):
+    return HttpResponse('<h1>About Blog')
