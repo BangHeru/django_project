fbc9ad7f931166a09d6c36e33f2e7d76fe4fad62
heru <bang.heru82@gmail.com>
2018-12-04 00:29:48 +0700

    add context to render to template

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,24 @@
 from django.shortcuts import render
 
-
+posts = [
+    {
+            'author': 'CoreyMS',
+            'title': 'Blog Post 1',
+            'content': 'First post content',
+            'date_posted': 'August 27, 2018'
+    },
+    {
+            'author': 'Jane Doe',
+            'title': 'Blog Post 2',
+            'content': 'Second post content',
+            'date_posted': 'August 28, 2018'
+    }
+]
 def home(req):
-    return render(req, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(req, 'blog/home.html', context)
 
 def about(req):
     return render(req, 'blog/about.html')
